39c4427113dbf89054f222462d483c705ec5a38f
Muhammad Fath Mushaffa Azhar <fathshaffa@gmail.com>
2023-04-02 10:47:10 +0700

    feat: add users models

---
+++ models/users.go
@@ -0,0 +1,14 @@
+package models
+
+import "time"
+
+type Users struct {
+	ID        string     `json:"id,omitempty"`
+	Username  string     `json:"username,omitempty"`
+	Password  string     `json:"password,omitempty"`
+	Email     string     `json:"email,omitempty"`
+	CardID    string     `json:"card_id,omitempty"`
+	SantriID  string     `json:"santri_id,omitempty"`
+	CreatedAt *time.Time `json:"created_at,omitempty"`
+	UpdatedAt *time.Time `json:"updated_at,omitempty"`
+}
