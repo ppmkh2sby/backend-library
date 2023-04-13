359908c7818ad052d4e7cdf5c06a49aa13746c5b
Muhammad Fath Mushaffa Azhar <fathshaffa@gmail.com>
2023-04-13 10:01:15 +0700

    refactor: add role in model users

--- models/users.go
+++ models/users.go
@@ -7,6 +7,7 @@ type Users struct {
 	Username  string     `json:"username,omitempty"`
 	Password  string     `json:"password,omitempty"`
 	Email     string     `json:"email,omitempty"`
+	Role      string     `json:"role,omitempty""`
 	CardID    string     `json:"card_id,omitempty"`
 	SantriID  string     `json:"santri_id,omitempty"`
 	CreatedAt *time.Time `json:"created_at,omitempty"`
